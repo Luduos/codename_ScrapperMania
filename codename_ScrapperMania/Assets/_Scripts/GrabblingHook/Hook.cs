0b7958ef230adb4a240ef4835118fe2e62403696
NoKnowledge <ke-messmer@gmx.de>
2018-12-19 02:04:47 +0100

    Created
    HookHolder (Contains the Hook)
    Hook (later HookModel)
    attached to Player
    
    Scripts added:
    Hook (handlesHookLogic)
    HookDetection( checks if we hit a HookableObj By Tag)
    
    Note Basic HookLogic isnt Completet yet
    We can shoot ourHook and it returns to the Character after reaching his maxDistance

---
+++ codename_ScrapperMania/Assets/_Scripts/GrabblingHook/Hook.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    
+    public GameObject hook;
+    public GameObject hookHolder;
+
+    public float hookTravelSpeed;
+    public float playerTravelSpeed;
+    public float maxDistance;
+    public float currentDistance;
+
+
+    private static bool ishooked;
+    public static bool fired;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0) && !fired)
+        {
+
+            fired = true;
+        }
+        if (fired)
+        {
+            //Move the Hook torwads pointed direction
+            hook.transform.Translate(Vector3.forward * Time.deltaTime * hookTravelSpeed);
+
+            // calculates the current traveldistance of the hook relative to the position of the Player
+            currentDistance = Vector3.Distance(transform.position, hook.transform.position);
+
+            if (currentDistance >= maxDistance)
+            {   
+                //called when the maxDistance of our Hook is reached
+                ReturnHook();
+            }
+        }
+        
+    }
+
+
+
+    void ReturnHook()
+    {
+        hook.transform.position = hookHolder.transform.position;
+        fired = false;
+        ishooked = false;
+
+    }
+
+    public bool getHooked()
+    {
+
+        return ishooked;
+    }
+
+    public void setHooked( bool updatedIsHooked)
+    {
+        ishooked = updatedIsHooked;
+    }
+}
---
+++ codename_ScrapperMania/Assets/_Scripts/GrabblingHook/HookDetection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookDetection : MonoBehaviour
+{
+    GameObject player;
+
+   void OnTriggerEnter(Collider other)
+    {
+        //Checks if we hit a hookableObj
+        if (other.tag.Equals("hookable"))
+        {  
+            //sets isHooked to true
+            player.GetComponent<Hook>().setIsHooked(true);
+
+        }
+    }
+}
